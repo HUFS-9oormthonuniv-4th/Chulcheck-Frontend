df90fc65a104e6078481ee84d98b4882a897e243
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-06-28 19:27:47 +0900

    feat: 오른쪽 아이콘 커스텀

--- src/components/ui/TitleAndDescription.tsx
+++ src/components/ui/TitleAndDescription.tsx
@@ -2,17 +2,21 @@ import React from "react";
 
 interface TitleAndDescriptionProps {
   title: string;
-  description: string;
+  description: React.ReactNode;
+  rightElement?: React.ReactNode;
 }
-
 export function TitleAndDescription({
   title,
   description,
+  rightElement,
 }: TitleAndDescriptionProps) {
   return (
     <div className="mb-2">
-      <h2 className="text-2xl font-extrabold text-[#1E293B] mb-1">{title}</h2>
-      <p className="text-base text-[#64748B]">{description}</p>
+      <div className="flex justify-between items-center">
+        <h2 className="text-2xl font-extrabold text-[#1E293B]">{title}</h2>
+        {rightElement && <div>{rightElement}</div>}
+      </div>
+      <p className="text-base text-[#64748B] mt-1">{description}</p>
     </div>
   );
 }
